9bb9375142c0b2810849c6b7aae42e990a10ce40
Aditi <aditidarade1@gmail.com>
2024-10-21 13:31:07 -0500

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Database<TKey, TValue>
+    {
+        private TKey[] keys;
+        private TValue[] values;
+        private int count = 0;
+        private int arraySize = 5;
+        public Database()
+        {
+            keys = new TKey[arraySize];
+            values = new TValue[arraySize];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+
+
+            if (count < arraySize)
+            {
+                this.keys[count] = key;
+                this.values[count] = value;
+                Console.WriteLine(keys[count] + ", " + values[count] + " added. Count:" + (count + 1));
+                count++;
+            }
+            else
+            {
+                arraySize = arraySize * 2;
+                TKey[] tempkeys = new TKey[arraySize];
+                TValue[] tempvalues = new TValue[arraySize];
+                //copy content of old array into new array
+                for (int i = 0; i < count; i++)
+                {
+                    tempkeys[i] = keys[i];
+                    tempvalues[i] = values[i];
+                }
+                //replace old arrays with new one 
+                keys = tempkeys;
+                values = tempvalues;
+
+                this.keys[count] = key;
+                this.values[count] = value;
+                Console.WriteLine(keys[count] + ", " + values[count] + " added after doubling the size of array. Count: " + (count + 1) + ", Array size now: " + arraySize);
+                count++;
+            }
+
+
+        }
+        // removes the key-value pair with the specified key from the database. Returns true if the pair was removed, false otherwise.
+        public bool Remove(TKey key)
+        {
+            bool result = false;
+            for (int i = 0; i < arraySize; i++)
+            {
+                if (key.Equals(keys[i]))
+                {
+                    Console.Write(keys[i] + ", " + values[i] + " removed.");
+                    for (int j = i; j < arraySize; j++)
+                    {
+                        if (arraySize - 1 == j)
+                        {
+                            //keys[j] = default(key);
+                            //values[j] = 0;
+                            break;
+                        }
+                        keys[j] = keys[j + 1];
+                        values[j] = values[j + 1];
+
+
+                    }
+                    count--;
+                    Console.WriteLine(" Count: " + count + ", Array size now: " + arraySize);
+                    Console.WriteLine("-------------------");
+                    //Print
+                    for (int k = 0; k < arraySize; k++)
+                    {
+                        Console.WriteLine(keys[k] + ", " + values[k]);
+                    }
+
+                    return result = true;
+                }
+            }
+            return result;
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+
+            bool result = false;
+            value = default(TValue);
+            for (int i = 0; i < count; i++)
+            {
+                if (keys[i].Equals(key))
+                {
+                    value = values[i];
+                    Console.WriteLine("Value of key " + key + " is: " + value);
+                    result = true;
+                }
+            }
+            if (result == false)
+            {
+                Console.WriteLine("There is no value associated with the specified key");
+            }
+            return result;
+        }
+        public int Count()
+        {
+
+            return count;
+
+        }
+
+        public void Swap<T>(ref T a, ref T b)
+        {
+            Console.WriteLine("Before Swap: Value of a: " + a + ", Value of b: " + b);
+            T temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("After Swap: Value of a: " + a + ", Value of b: " + b);
+        }
+    }
+}
---
+++ Sec21_Assign44_CustomGenericClasses/Program.cs
@@ -0,0 +1,55 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sec21_Assign44_CustomGenericClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int ArraySize = 10;
+            Database<int, int> d1 = new Database<int, int>();
+            //d1.keys[0] = 1; 
+            d1.Add(1,2);
+            d1.Add(2,3);
+            d1.Add(3,4);
+            d1.Add(4,5);
+            d1.Add(5,3);
+            d1.Add(6,4);
+            d1.Add(7,5);
+            Console.WriteLine("--------------------");
+
+            d1.Remove(3);            
+            Console.WriteLine("--------------------");
+
+            int i;
+            d1.TryGetValue(1, out i);
+            Console.WriteLine("--------------------");
+
+            Console.WriteLine("Count: " + d1.Count());
+            Console.WriteLine("--------------------");
+
+
+            int a = 1;
+            int b = 2;
+            d1.Swap(ref a, ref b);
+
+            Database<string, int> d2 = new Database<string, int>();
+            string f = "Bill ";
+            int g = 60;
+            //d2.Swap(ref f, ref g);
+
+            Database<string, string> d3 = new Database<string, string>();
+            string r = "Bill ";
+            string t = "Gates";
+            d3.Swap<string>(ref r, ref t);
+
+
+            Console.ReadKey();
+        }
+    }
+}
